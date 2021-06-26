5d0b94bd176fa3c80dedee526d9eed4fe279b908
swapnil.s <swapnil.s@TEMP-SWAPNLS.local>
2021-06-27 00:33:35 +0530

    added icons and updated tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,9 +3,10 @@
 This changelog follows [the Keep a Changelog standard](https://keepachangelog.com).
 
 
-## [Unreleased](https://github.com/blade-organization/blade-heroicons/compare/1.0.0...main)
+## [Unreleased](https://github.com/codeat3/blade-elusive-icons/compare/1.0.0...main)
 
 
-## 1.0.0 (20??-??-??)
+## 1.0.0 (2021-06-27)
 
-Initial release.
+* Initial release.
+* synced with the latest commit [5ddd9167c8479ec07b78f3ba7150dc4ad32cc7bc](https://github.com/dovy/elusive-icons/commit/5ddd9167c8479ec07b78f3ba7150dc4ad32cc7bc) - although it was on 20 Feb 2015
--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
+use Codeat3\BladeElusiveIcons\BladeElusiveIconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
 use Orchestra\Testbench\TestCase;
 
@@ -13,13 +13,11 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_compiles_a_single_anonymous_component()
     {
-        $result = svg('heroicon-o-bell')->toHtml();
+        $result = svg('elusive-bulb')->toHtml();
 
         // Note: the empty class here seems to be a Blade components bug.
         $expected = <<<'SVG'
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
-            </svg>
+            <svg viewBox="0 0 1200 1200"><path d="M600 119.608c-158.775 0-287.486 96.415-287.486 215.368l163.322 494.645h248.328l163.322-494.659c0-118.951-128.702-215.369-287.486-215.369v.015zm0 46.996c158.775 0 240.479 128.138 240.479 168.362L724.161 680.393H475.833L359.515 334.966c0-49.63 81.704-168.362 240.479-168.362H600zM472.215 865.699v85.982h255.57v-85.984h-255.57v.002zm0 128.725v85.982h255.57v-85.982h-255.57z" fill="currentColor"/></svg>
             SVG;
 
         $this->assertSame($expected, $result);
@@ -28,12 +26,10 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_can_add_classes_to_icons()
     {
-        $result = svg('heroicon-o-bell', 'w-6 h-6 text-gray-500')->toHtml();
+        $result = svg('elusive-bulb', 'w-6 h-6 text-gray-500')->toHtml();
 
         $expected = <<<'SVG'
-            <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
-            </svg>
+            <svg class="w-6 h-6 text-gray-500" viewBox="0 0 1200 1200"><path d="M600 119.608c-158.775 0-287.486 96.415-287.486 215.368l163.322 494.645h248.328l163.322-494.659c0-118.951-128.702-215.369-287.486-215.369v.015zm0 46.996c158.775 0 240.479 128.138 240.479 168.362L724.161 680.393H475.833L359.515 334.966c0-49.63 81.704-168.362 240.479-168.362H600zM472.215 865.699v85.982h255.57v-85.984h-255.57v.002zm0 128.725v85.982h255.57v-85.982h-255.57z" fill="currentColor"/></svg>
             SVG;
 
         $this->assertSame($expected, $result);
@@ -42,12 +38,10 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_can_add_styles_to_icons()
     {
-        $result = svg('heroicon-o-bell', ['style' => 'color: #555'])->toHtml();
+        $result = svg('elusive-bulb', ['style' => 'color: #555'])->toHtml();
 
         $expected = <<<'SVG'
-            <svg style="color: #555" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
-            </svg>
+            <svg style="color: #555" viewBox="0 0 1200 1200"><path d="M600 119.608c-158.775 0-287.486 96.415-287.486 215.368l163.322 494.645h248.328l163.322-494.659c0-118.951-128.702-215.369-287.486-215.369v.015zm0 46.996c158.775 0 240.479 128.138 240.479 168.362L724.161 680.393H475.833L359.515 334.966c0-49.63 81.704-168.362 240.479-168.362H600zM472.215 865.699v85.982h255.57v-85.984h-255.57v.002zm0 128.725v85.982h255.57v-85.982h-255.57z" fill="currentColor"/></svg>
             SVG;
 
         $this->assertSame($expected, $result);
@@ -57,7 +51,7 @@ class CompilesIconsTest extends TestCase
     {
         return [
             BladeIconsServiceProvider::class,
-            BladeHeroiconsServiceProvider::class,
+            BladeElusiveIconsServiceProvider::class,
         ];
     }
 }
