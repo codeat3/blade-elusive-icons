7a83e154e81cc94c7db56d75746ffedfa671ee7c
Swapnil Sarwe <swapnilsarwe@gmail.com>
2023-02-18 19:00:22 +0530

    laravel 10 support (#2)
    * laravel 10 support

--- src/BladeElusiveIconsServiceProvider.php
+++ src/BladeElusiveIconsServiceProvider.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Codeat3\BladeElusiveIcons;
 
 use BladeUI\Icons\Factory;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\ServiceProvider;
 
 final class BladeElusiveIconsServiceProvider extends ServiceProvider
 {
@@ -17,24 +17,24 @@ final class BladeElusiveIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-elusive-icons', []);
 
-            $factory->add('elusive-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('elusive-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-elusive-icons.php', 'blade-elusive-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-elusive-icons.php', 'blade-elusive-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-elusive-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-elusive-icons'),
             ], 'blade-elusive-icons');
 
             $this->publishes([
-                __DIR__.'/../config/blade-elusive-icons.php' => $this->app->configPath('blade-elusive-icons.php'),
+                __DIR__ . '/../config/blade-elusive-icons.php' => $this->app->configPath('blade-elusive-icons.php'),
             ], 'blade-elusive-icons-config');
         }
     }
