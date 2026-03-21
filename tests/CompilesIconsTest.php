a0007f7ce2238bb27953d9488a52ab35d99a0929
Swapnil Sarwe <swapnilsarwe@gmail.com>
2026-03-21 17:57:50 +0530

    laravel 13 compatibility (#16)

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -11,7 +11,7 @@ use Orchestra\Testbench\TestCase;
 class CompilesIconsTest extends TestCase
 {
     /** @test */
-    public function it_compiles_a_single_anonymous_component()
+    public function test_it_compiles_a_single_anonymous_component()
     {
         $result = svg('elusive-bulb')->toHtml();
 
@@ -24,7 +24,7 @@ class CompilesIconsTest extends TestCase
     }
 
     /** @test */
-    public function it_can_add_classes_to_icons()
+    public function test_it_can_add_classes_to_icons()
     {
         $result = svg('elusive-bulb', 'w-6 h-6 text-gray-500')->toHtml();
 
@@ -36,7 +36,7 @@ class CompilesIconsTest extends TestCase
     }
 
     /** @test */
-    public function it_can_add_styles_to_icons()
+    public function test_it_can_add_styles_to_icons()
     {
         $result = svg('elusive-bulb', ['style' => 'color: #555'])->toHtml();
 
